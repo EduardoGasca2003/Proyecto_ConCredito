94befa2f1cd6ac054916a8074b0846c3726fc5fa
EduardoGasca2003 <eduardogasca.contacto@gmail.com>
2025-03-11 00:26:45 -0700

    Navbar implementada

--- frontend/src/Home.js
+++ frontend/src/Home.js
@@ -1,37 +1,33 @@
 import React, { useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
 import axios from 'axios';
+import Navbar from './componentes/Navbar';
 
 function Home() {
-    const navigate = useNavigate();
+  const navigate = useNavigate();
 
-    useEffect(() => {
-      const token = localStorage.getItem('token');
-      if(!token){
-        navigate('/');
-      } else {
-        axios.get('http://localhost:8080/home', {
-          headers: { Authorization: token }
-        })
-        .then(res => console.log(res.data))
-        .catch(err => {
-          console.log(err);
-          navigate('/');
-        });
-      }
-    }, []);
-  
-    const handleLogout = () => {
-      localStorage.removeItem('token');
+  useEffect(() => {
+    const token = localStorage.getItem('token');
+    if(!token){
       navigate('/');
+    } else {
+      axios.get('http://localhost:8080/home', {
+        headers: { Authorization: token }
+      })
+      .then(res => console.log(res.data))
+      .catch(err => {
+        console.log(err);
+        navigate('/');
+      });
     }
-  
-    return (
-      <div>
-        <h1>Bienvenido a tu lista de tareas</h1>
-        <button onClick={handleLogout}>Cerrar sesión</button>
-      </div>
-    );
-  }
+  }, []);
+
+  return (
+    <div>
+      <Navbar />
+      <h1>Bienvenido a tu lista de tareas</h1>
+    </div>
+  );
+}
 
 export default Home;
---
+++ frontend/src/componentes/Navbar.js
@@ -0,0 +1,50 @@
+import React from 'react';
+import { Link, useNavigate } from 'react-router-dom';
+
+function Navbar() {
+  const navigate = useNavigate();
+
+  const handleLogout = () => {
+    localStorage.removeItem('token');
+    navigate('/');
+  }
+
+  return (
+    <nav className="navbar navbar-expand-lg navbar-light bg-light">
+      <div className="container-fluid">
+        <Link className="navbar-brand" to="/home">Navbar</Link>
+        <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="collapse navbar-collapse" id="navbarNavDropdown">
+          <ul className="navbar-nav">
+            <li className="nav-item">
+              <Link className="nav-link active" to="/home">Home</Link>
+            </li>
+            <li className="nav-item">
+              <Link className="nav-link" to="/features">Features</Link>
+            </li>
+            <li className="nav-item">
+              <Link className="nav-link" to="/pricing">Pricing</Link>
+            </li>
+            <li className="nav-item dropdown">
+              <a className="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                Dropdown link
+              </a>
+              <ul className="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+                <li><a className="dropdown-item" href="#">Action</a></li>
+                <li><a className="dropdown-item" href="#">Another action</a></li>
+                <li><a className="dropdown-item" href="#">Something else here</a></li>
+              </ul>
+            </li>
+            <li className="nav-item">
+              <button className="btn btn-danger" onClick={handleLogout}>Cerrar sesión</button>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+  );
+}
+
+export default Navbar;
